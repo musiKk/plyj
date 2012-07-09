6c0b2ef3649b5f624182e92984062d1c0c8efa2c
Werner Hahn <werner_hahn@gmx.com>
2012-07-09 18:36:43 +0200

    create a python package proper
    may still be improvable, I'm new at this

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='plyj',
+    version='0.0.1',
+    author='Werner Hahn',
+    author_email='werner_hahn@gmx.com',
+    packages=['plyj'],
+    url='http://github.com/musiKk/plyj',
+    license='COPYING',
+    description='A Java parser written in Python using PLY. ',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "ply >= 3.4",
+    ],
+)
