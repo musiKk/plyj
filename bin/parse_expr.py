5e56a60d35f585c740c3f25a7f932a1be129f92d
Werner Hahn <werner_hahn@gmx.com>
2012-07-16 21:59:42 +0200

    add sample program to parse expression from CLI

---
+++ bin/parse_expr.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import sys
+
+if len(sys.argv) == 1:
+    print '''usage: parse_expr.py <expression> ...
+   Example: parse_expr.py '1+2' '3' 'j = (int) i + 3' '''
+    sys.exit(1)
+
+import plyj.parser as plyj
+
+parser = plyj.Parser()
+for expr in sys.argv[1:]:
+    print parser.parse_expression(expr)
+
