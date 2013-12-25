3692177c76756d97be4445a8f4d983b57d3d38c2
Werner Hahn <werner_hahn@gmx.com>
2013-12-25 15:27:25 +0100

    add package and import tests

--- test/compilation_unit.py
+++ test/compilation_unit.py
@@ -8,26 +8,91 @@ class CompilationUnitTest(unittest.TestCase):
     def setUp(self):
         self.parser = plyj.Parser()
 
-    def testAnnotations(self):
+    def test_class(self):
+        m = self.parser.parse_string('''
+        class Foo {}
+        ''')
+        cls = self._assert_declaration(m, 'Foo')
+        self.assertEqual(len(cls.modifiers), 0)
+
+    def test_class_modifiers(self):
+        m = self.parser.parse_string('''
+        public static final class Foo {}
+        ''')
+        cls = self._assert_declaration(m, 'Foo')
+        self.assertEqual(cls.modifiers, ['public', 'static', 'final'])
+
+    def test_default_package(self):
+        m = self.parser.parse_string('''
+        class Foo {}
+        ''')
+        self.assertIsNone(m.package_declaration)
+
+    def test_package(self):
+        m = self.parser.parse_string('''
+        package foo.bar;
+        ''')
+        self.assertEqual(m.package_declaration, model.PackageDeclaration(model.Name('foo.bar')))
+
+    def test_package_annotation(self):
+        m = self.parser.parse_string('''
+        @Annot
+        package foo;
+        ''')
+        self.assertEqual(m.package_declaration,
+                         model.PackageDeclaration(model.Name('foo'),
+                                                  modifiers=[model.Annotation(model.Name('Annot'))]))
+
+    def test_import(self):
+        m = self.parser.parse_string('''
+        import foo;
+        import foo.bar;
+        ''')
+        self.assertEqual(m.import_declarations,
+                         [model.ImportDeclaration(model.Name('foo')),
+                          model.ImportDeclaration(model.Name('foo.bar'))])
+
+    def test_static_import(self):
+        m = self.parser.parse_string('''
+        import static foo.bar;
+        ''')
+        self.assertEqual(m.import_declarations,
+                         [model.ImportDeclaration(model.Name('foo.bar'), static=True)])
+
+    def test_wildcard_import(self):
+        m = self.parser.parse_string('''
+        import foo.bar.*;
+        ''')
+        self.assertEqual(m.import_declarations,
+                         [model.ImportDeclaration(model.Name('foo.bar'), on_demand=True)])
+
+    def test_static_wildcard_import(self):
+        m = self.parser.parse_string('''
+        import static foo.bar.*;
+        ''')
+        self.assertEqual(m.import_declarations,
+                         [model.ImportDeclaration(model.Name('foo.bar'), static=True, on_demand=True)])
+
+    def test_annotations(self):
         # bug #13
         m = self.parser.parse_string('''
         @Annot(key = 1)
         class Foo {}
         ''')
-        self.assertIsInstance(m, model.CompilationUnit)
-        self.assertEqual(len(m.type_declarations), 1)
+        t = self._assert_declaration(m, 'Foo')
 
-        t = m.type_declarations[0]
-        self.assertEqual(len(t.modifiers), 1)
+        self.assertEqual(t.modifiers, [model.Annotation(
+            name=model.Name('Annot'),
+            members=[model.AnnotationMember(name=model.Name('key'),
+                                            value=model.Literal('1'))])])
 
-        annot = t.modifiers[0]
-        self.assertIsInstance(annot, model.Annotation)
+    def _assert_declaration(self, compilation_unit, name, index=0, type=model.ClassDeclaration):
+        self.assertIsInstance(compilation_unit, model.CompilationUnit)
+        self.assertTrue(len(compilation_unit.type_declarations) >= index + 1)
 
-        self.assertEqual(len(annot.members), 1)
-        member = annot.members[0]
+        decl = compilation_unit.type_declarations[index]
+        self.assertIsInstance(decl, type)
 
-        self.assertIsInstance(member, model.AnnotationMember)
+        self.assertEqual(decl.name, name)
 
-        self.assertEqual(member, model.AnnotationMember(
-            name=model.Name('key'),
-            value=model.Literal('1')))
+        return decl
