d3ddb6f3cab8204a399849df00500ccf1ea0cdda
Werner Hahn <werner_hahn@gmx.com>
2014-12-25 21:59:39 +0100

    version 0.1, create MANIFEST.in, update classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='plyj',
-    version='0.0.1',
+    version='0.1',
     author='Werner Hahn',
     author_email='werner_hahn@gmx.com',
     packages=['plyj'],
@@ -10,6 +10,19 @@ setup(
     license='COPYING',
     description='A Java parser written in Python using PLY. ',
     long_description=open('README.md').read(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Education',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing'
+    ],
     install_requires=[
         "ply >= 3.4",
     ],
